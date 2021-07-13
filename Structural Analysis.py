70fe9aabd45b31c32a8a262a21acb62d23ffbd46
Agyeya Mishra <53916781+AgyeyaMishra@users.noreply.github.com>
2021-07-13 12:53:56 +0530

    Update 4

--- Structural Analysis.py
+++ Structural Analysis.py
@@ -189,7 +189,120 @@ while(choice == 'y'):
 
     # If the type of beam selected is propped cantilever beam then the following if statement will be executed
     elif type_of_beam == '2':
-        print('not ready')
+        print("You have selected Propped Cantilever Beam.")
+
+        # Loading conditions menu starts here
+        print("\nThe various types of loading conditions available for propped cantilever beam are : ")
+
+        # Point Load for propped cantilever beam
+        print("\n1:-> Point Load :- ")
+        print("                   |P")
+        print("                   |")
+        print("                   V")
+        print("       |----------------------")
+        print("                             O")
+        print("       A           B         C")
+        print("\n")
+        
+        # Uniformly Distributed Load for propped cantilever beam
+        print("2:-> Uniformly Distributed Load :- ")
+        print("                  |W")
+        print("                  |")
+        print("                  V")
+        print("        /\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\")
+        print("       |----------------------")
+        print("                             O")
+        print("       A          B          C")
+        print("\n")
+
+        # Moment Load for propped cantilever beam
+        print("3:-> Moment Load :- ")
+        print("                             \\M")
+        print("       |----------------------\\")
+        print("                             O/")
+        print("                             L")
+        print("       A           B         C")
+
+        # User is asked to select the type of loading condition her or she wishes to apply to the beam
+        load_proppedcantilever = input("Please specify the loading condition (1, 2 or 3) : ")
+
+        # If the loading condition selected is Point Load then the following if statement will be executed
+        if load_proppedcantilever == '1':
+            print("You have selected Point Load as the loading condition.")
+
+            # User is asked to enter values for load, length of beam, modulus of elasticity and moment of inertia
+            P = input("\nEnter load magnitude (P) in N : ")
+            L = input("Enter length of beam (L) in mm : ")
+            E = input("Enter modulus of elasticity (E) of the material of beam in N/mm^2 : ")
+            I = input("Enter moment of inertia (I) of beam in mm^4 : ")
+            
+            # Menu for various structural analysis methods starts here
+            print("\nThe various methods of analysis available are : ")
+            print("1:-> Strain Energy Method")
+            print("2:-> Minimum Potential Energy Theorem Method")
+            print('method3')
+            
+            # User is asked to select the method of structural analysis
+            analysis_proppedcantilever = input("\nPlease select the method of analysis (1, 2 or 3) : ")
+            
+            # If the method of analysis selected is Strain Energy Method then the following if statement will be executed
+            if analysis_proppedcantilever == '1':
+                print("You have selected Strain Energy Method.")
+
+                # This method is not ready right now.
+            
+            # If the method of analysis selected is Minimum Energy Theorem Method then the following if statement will be executed
+            elif analysis_proppedcantilever == '2':
+                print("You have selected Minimum Potentital Energy Theorem Method.")
+
+                print("\nApproach:- We will first find redundant for the two the spans AB and BC and then we will superimpose them to find the redundant for the complete beam. ")
+                redundantAB = (float(P)*5)/14
+                print("Redundant reaction for span AB = R\' = 5P/14 = " + str(redundantAB))
+                print("Redundant reaction for span BC = R\" = 0")
+                print("Therefore, total redundant reaction for beam AC = R = R\' + R\" = " + str(redundantAB))
+
+            else:
+                print('not ready')
+
+        # If the type of loading condition selected is uniformly distributed load then the following elif statement will be executed
+        elif load_proppedcantilever == '2':
+            print("You have selected Uniformly Distributed Load as the loading condition.")
+            
+            # User is asked to enter values for uniformly distributed load, length of beam, modulus of elasticity and moment of inertia
+            W = input("\nEnter uniformly distributed load (w) in N/mm : ")
+            L = input("Enter length of beam (L) in mm : ")
+            E = input("Enter modulus of elasticity (E) of the material of beam in N/mm^2 : ")
+            I = input("Enter moment of inertia (I) of beam in mm^4 : ")
+
+            # Menu for various structural analysis methods starts here
+            print("\nThe various methods of analysis available are : ")
+            print("1:-> Strain Energy Method")
+            print("2:-> Minimum Potential Energy Theorem Method")
+            print('method3')
+
+            # User is asked to select the method of structural analysis
+            analysis_propepdcantilever = input("\nPlease select the method of analysis (1, 2 or 3) : ")
+
+            # If the method of analysis selected is Strain Energy Method then the following if statement will be executed
+            if analysis_proppedcantilever == '1':
+                print("You have selected Strain Energy Method.")
+
+            # This method is not ready right now.
+
+            # If the method of analysis selected is Minimum Potential Energy Method then the following elif statement will be executed
+            elif analysis_proppedcantilever == '2':
+                print("You have selected Minimum Potential Energy Method.")
+
+                print("\nApproach:- We will get the true value of redundant reaction when the total strain energy in the beam will be minimum.")
+                redundant = (3*int(W)*int(L))/8
+                print("Redundant reaction in the beam = R = 3WL/8 = " + str(redundant))
+
+            else:
+                print('not ready')
+
+        # If the type of loading condition selected is moment load then the following else statement will be executed
+        else:
+            print('not ready')
 
     # If the type of beam selected is simply supported beam then the following if statement will be executed
     elif type_of_beam == '3':
@@ -345,9 +458,50 @@ while(choice == 'y'):
                 print('not ready')
                 
                             
-        
+    # If the type of beam selected is fixed beam then the following else statement will be executed    
     else:
-        print('not ready')
+        print('specify the loading conditions')
+        print('1) Point Load :      |P     ')
+        print('                     V')
+        print('             A|-------------|B')
+        print('\n \n')
+        print('2)UDL         :      W        ')
+        print('              /\\/\\/\\/\\/\\/\\/\\/\\')
+        print('            A|----------------|B')
+        load = input('load type (1/2/) : ')
+        if load == '1':
+            P = input('P : ')
+            l = input('length of beam : ')
+            print()
+            print('selecting the method to be used')
+            print('1) Strain Energy Method')
+            print('2) ')
+            print('3) Consistent Deformation Method')
+            print()
+            method = input('method to be used (1/2/3) : ')
+            if method == '1':
+                print('not ready')
+            elif method == '2':
+                print('not ready')
+            else:
+                print('q')
+
+        else:
+            W = input('W : ')
+            l = input('length of beam : ')
+            print()
+            print('selecting the method to be used')
+            print('1) Strain Energy Method')
+            print('2) ')
+            print('3) Consistent Deformation Method')
+            print()
+            method = input('method to be used (1/2/3) : ')
+            if method == '1':
+                print('not ready')
+            elif method == '2':
+                print('not ready')
+            else:
+                print('q')
 
 # If the user doesn't wish to continue with the program for beam analysis then the following else statement will be executed
 else:
